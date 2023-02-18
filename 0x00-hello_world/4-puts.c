07902927691e75d4c22d76f1f9bad41becf22c3c
Yimiika <oluwafemiaina8@gmail.com>
2023-02-18 09:21:25 -0800

    M

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - entry point for the program
+ *
+ * Description: Prints a message using the puts function
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
