09efcfccfa2db672de4ae528fb938562f08d665b
Yimiika <oluwafemiaina8@gmail.com>
2023-02-22 10:10:03 -0800

    Hey

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,7 @@
  */
 int print_last_digit(int n)
 {
-  int last_digit;
-
-  if (n < 0)
-    n = -n;
-
-  last_digit = n % 10;
-
+  int last_digit = n % 10;
   _putchar(last_digit + '0');
-  _putchar('\n');
-
-  return (last_digit);
+  return last_digit;
 }
