c56c68c28a7be413ded7f427c27c209eb6e98631
Yimiika <oluwafemiaina8@gmail.com>
2023-02-22 11:15:31 -0800

    Hey

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,26 +6,27 @@
  *
  * Return: void
  */
-
 void print_times_table(int n)
 {
 int i, j, result;
-if (n < 0 || n > 15)
-return;
-for (int i = 0; i <= n; i++)
+void print_times_table(int n)
 {
-for (int j = 0; j <= n; j++)
-{
-int result = i * j;
-if (j == 0)
-printf("%d", result);
-else if (result < 10)
-printf(",   %d", result);
-else if (result < 100)
-printf(",  %d", result);
-else
-printf(", %d", result);
-}
-printf("\n");
-}
+    int i, j, result;
+
+    if (n < 0 || n > 15)
+        return;
+
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            result = i * j;
+            if (j == 0)
+                printf("%d", result);
+            else
+                printf(", %d", result < 10 ? 32 : result < 100 ? 0 : result / 100);
+            printf("%d", result < 10 ? result : result < 100 ? result % 10 : (result / 10) % 10);
+        }
+        printf("\n");
+    }
 }
