5aef92545a1f6e67654f71e032037d2db3c072bb
Yimiika <oluwafemiaina8@gmail.com>
2023-02-22 03:07:00 -0800

    Test

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * print_sign - prints the sign of a number
  *
@@ -8,19 +9,19 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
 }
