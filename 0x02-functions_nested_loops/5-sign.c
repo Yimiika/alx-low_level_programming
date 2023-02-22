43e18889ed91083b132671f878dc40321c372562
Yimiika <oluwafemiaina8@gmail.com>
2023-02-22 04:06:58 -0800

    Test

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,19 @@ int print_sign(int n)
 if (n > 0)
 {
 putchar('+');
+putchar('\n');
 return (1);
 }
 else if (n == 0)
 {
+putchar('0');
+putchar('\n');
 return (00);
 }
 else
 {
 putchar('-');
+putchar('\n');
 return (1);
 }
 }
