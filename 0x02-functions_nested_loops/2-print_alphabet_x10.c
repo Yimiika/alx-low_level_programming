3e6c83f33e8be770e16882d05b67c0f34c6c68c1
Yimiika <oluwafemiaina8@gmail.com>
2023-02-22 02:09:35 -0800

    Test

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * print_alphabet_x10 - Prints the lowercase alphabet ten times followed by a new line
+ * print_alphabet_x10 - Prints the lowercase alphabet ten times
  */
 void print_alphabet_x10(void)
 {
