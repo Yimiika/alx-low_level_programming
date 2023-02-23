bb047231823e8898e1b2d1cdfb7d2711a551f4a8
Yimiika <oluwafemiaina8@gmail.com>
2023-02-22 23:29:42 -0800

    New debugging directory

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - demonstrates an infinite loop
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+/* while (i < 10)
+{
+} */
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
