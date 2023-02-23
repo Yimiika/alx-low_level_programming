93207838b91b8bf1d4bce12606a2c9659c62045b
Yimiika <oluwafemiaina8@gmail.com>
2023-02-22 23:24:26 -0800

    New debugging directory

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 int main(void)
 {
-int i = 0, j = 98, k = -10;
-positive_or_negative(i);
-positive_or_negative(j);
-positive_or_negative(k);
+int i;
+i = 0, positive_or_negative(i);
+i = 98, positive_or_negative(i);
+ = -10, positive_or_negative(i);
 return (0);
 }
