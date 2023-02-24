9449eb9b405cd05d1a9f6b84151887dd31f4ec8c
Yimiika <oluwafemiaina8@gmail.com>
2023-02-24 00:49:35 -0800

    Message

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,20 +7,19 @@
  */
 int main(void)
 {
-    long int n = 612852475143;
-    long int i;
-
-    for (i = 2; i <= n; i++)
-    {
-        while (n % i == 0)
-        {
-            n /= i;
-            if (n == 1 || n == i)
-            {
-                printf("%ld\n", i);
-                return (0);
-            }
-        }
-    }
-    return (0);
+long int n = 612852475143;
+long int i;
+for (i = 2; i <= n; i++)
+{
+while (n % i == 0)
+{
+n /= i;
+if (n == 1 || n == i)
+{
+printf("%ld\n", i);
+return (0);
+}
+}
+}
+return (0);
 }
