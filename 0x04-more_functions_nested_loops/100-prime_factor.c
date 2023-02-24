cc1220040b095d66df8002d1225d84dbe4e55af4
Yimiika <oluwafemiaina8@gmail.com>
2023-02-24 00:44:27 -0800

    Message

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    long int n = 612852475143;
+    long int i;
+
+    for (i = 2; i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            n /= i;
+            if (n == 1 || n == i)
+            {
+                printf("%ld\n", i);
+                return (0);
+            }
+        }
+    }
+    return (0);
+}
