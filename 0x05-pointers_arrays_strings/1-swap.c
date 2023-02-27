2d2788d79b18e87f3eb31e7902bc930cc115ad77
Yimiika <oluwafemiaina8@gmail.com>
2023-02-27 09:50:24 -0800

    Coding

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer in function
+ * @b: integer in function
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+*a = *b;
+*b = *a;
+}
