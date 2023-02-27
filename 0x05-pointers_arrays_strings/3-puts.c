9086edc100d8ba872feb43f052d278bde1c807dc
Yimiika <oluwafemiaina8@gmail.com>
2023-02-27 10:44:54 -0800

    Coding

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: integer of function
+ * 
+ *
+ * Return: The result of count
+ */
+void _puts(char *str)
+{
+for (*str = 'Z'; *str >= 'A'; *str--) 
+{
+putchar(*str \n);
+}
+return (0);
+}
