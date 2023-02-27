faa068697401d7d6293a524d5f2e5b6daafb0111
Yimiika <oluwafemiaina8@gmail.com>
2023-02-27 13:52:09 -0800

    Coding

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void puts2(char *str)
+        {
+int i;
+           string result = " ";
+            for (i = 0; i < str.length(); i++)
+             {
+                 if (i % 2 == 0) result += str[i];
+             }
+            return result;
+        }
