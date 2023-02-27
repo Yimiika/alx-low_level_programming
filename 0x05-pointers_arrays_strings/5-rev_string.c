b6e03eba8fd3d5414bc7a10dc22b5e23b1408a88
Yimiika <oluwafemiaina8@gmail.com>
2023-02-27 13:21:47 -0800

    Coding

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,44 +1,27 @@
 #include <stdio.h>
-
 #include <string.h>
 
 /**
  * rev_string - reverses a string
- * 
+ * @s: character in string 
  * 
  */
 
 void rev_string(char *s)
-
 {
-
 int l, i;
-
 char *begin_ptr, *end_ptr, ch;
-
-
 l = strlen(s);
-
 begin_ptr = s;
-
 end_ptr = s;
-
 for (i = 0; i < l - 1; i++)
-
 end_ptr++;
-
 for (i = 0; i < l / 2; i++) 
 {
 ch = *end_ptr;
-
 *end_ptr = *begin_ptr;
-
 *begin_ptr = ch;
-
 begin_ptr++;
-
 end_ptr--;
-
 }
-
 }
