c5aa664a647649741a3cc8d68c3980513b07311b
Yimiika <oluwafemiaina8@gmail.com>
2023-02-28 10:01:32 -0800

    Coding

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <limits.h>
- 
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string
+ *
+ * Return: the result
+ */
 int _atoi(char *s)
 {int sign = 1, base = 0, i = 0;
 while (s[i] == ' ')
@@ -18,11 +24,11 @@ if (base > INT_MAX / 10
 && s[i] - '0' > 7))
 {
 if (sign == 1)
-return INT_MAX;
+return (INT_MAX);
 else
-return INT_MIN;
+return (INT_MIN);
 }
-base = 10 * base + (s[i++] - '0');
+base = 10 *base + (s[i++] - '0');
 }
-return base * sign;
+return (base *sign);
 }
