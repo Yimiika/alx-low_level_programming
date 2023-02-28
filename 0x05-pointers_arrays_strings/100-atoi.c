fb7ccc6f1cd6db944d7ff1a1ac828e149a7e03f9
Yimiika <oluwafemiaina8@gmail.com>
2023-02-28 09:47:06 -0800

    Coding

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int _atoi(char *s)
+{
+int res = 0;
+int sign = 1;
+int i = 0;
+if (str[0] == '-')
+{
+sign = -1;
+i++;
+}
+for (; str[i] != '\0'; ++i)
+res = res * 10 + str[i] - '0';
+return sign * res;
+}
