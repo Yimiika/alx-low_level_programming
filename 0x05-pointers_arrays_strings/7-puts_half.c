2e136b12a5bf2f3d5cbcd7fad23c05654b4274ad
Yimiika <oluwafemiaina8@gmail.com>
2023-02-28 10:40:43 -0800

    Coding

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *puts_half - function that prints half of a string.
+ *@str: input string
+ */
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	j = i / 2;
+
+	if (i % 2 == 1)
+		j++;
+
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	putchar('\n');
+}
