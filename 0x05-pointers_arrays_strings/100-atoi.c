dd2fffe78d484c9f7d7d503cc6a374be7fba4b04
Yimiika <oluwafemiaina8@gmail.com>
2023-02-28 09:54:33 -0800

    Coding

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
-
-
+#include <limits.h>
+ 
 int _atoi(char *s)
+{int sign = 1, base = 0, i = 0;
+while (s[i] == ' ')
 {
-int res = 0;
-int sign = 1;
-int i = 0;
-if (s[0] == '-')
-{
-sign = -1;
 i++;
 }
-for (; s[i] != '\0'; ++i)
-res = res * 10 + s[i] - '0';
-return sign * res;
+if (s[i] == '-' || s[i] == '+')
+{
+sign = 1 - 2 * (s[i++] == '-');
+}
+while (str[i] >= '0' && s[i] <= '9')
+{
+if (base > INT_MAX / 10
+|| (base == INT_MAX / 10
+&& s[i] - '0' > 7))
+{
+if (sign == 1)
+return INT_MAX;
+else
+return INT_MIN;
+}
+base = 10 * base + (s[i++] - '0');
+}
+return base * sign;
 }
