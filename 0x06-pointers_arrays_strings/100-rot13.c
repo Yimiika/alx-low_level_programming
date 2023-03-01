34a612cac31700b22421c5b237de9423d69abe23
Yimiika <oluwafemiaina8@gmail.com>
2023-03-01 12:39:23 -0800

    Coding

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,20 +1,28 @@
+#include <stdio.h>
+
+/**
+ * rot13 - Encodes a string using rot13
+ *
+ * @s: The string to encode
+ *
+ * Return: A pointer to the encoded string
+ */
 char *rot13(char *s)
 {
-  int i, j;
-  char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-  
-  for (i = 0; s[i]; i++)
-  {
-    for (j = 0; input[j]; j++)
-    {
-      if (s[i] == input[j])
-      {
-        s[i] = output[j];
-        break;
-      }
-    }
-  }
-  
-  return s;
+	int i, j;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; alpha[j]; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
