2b322e2066b67a8bb19799222aecf8dcb4401414
Yimiika <oluwafemiaina8@gmail.com>
2023-03-01 12:22:29 -0800

    Coding

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,18 +10,18 @@
 
 char *leet(char *changed)
 {
-	int i, j;
+	int index, j;
 	char minus[] = {'a', 'e', 'o', 't', 'l', '\0'};
 	char mayus[] = {'A', 'E', 'O', 'T', 'L', '\0'};
 	char numbers[] = {'4', '3', '0', '7', '1', '\0'};
 
-	for (i = 0; changed[i] != '\0'; ++in)
+	for (index = 0; changed[index] != '\0'; ++index)
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if (changed[i] == minus[j] || changed[i] == mayus[j])
+			if (changed[index] == minus[j] || changed[index] == mayus[j])
 			{
-				changed[i] = numbers[j];
+				changed[index] = numbers[j];
 			}
 		}
 	}
