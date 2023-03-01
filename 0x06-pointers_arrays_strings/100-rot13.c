a96bdde98e52f73759fb08134d5821d84fc75b54
Yimiika <oluwafemiaina8@gmail.com>
2023-03-01 12:35:38 -0800

    Coding

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,35 +1,20 @@
-#include <stdio.h>
-
-/**
- * cap_string - Write a function that capitalizes all words of a string.
- *
- * @entry: This is the input string
- *
- * Return: String capitalized
- */
-char *cap_string(char *entry)
+char *rot13(char *s)
 {
-	int conversion, index, count;
-
-	char chars[] = {' ', ',', ';', '.', '!',
-			 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
-	conversion = 32;
-
-	for (index = 0; entry[index] != '\0'; index++)
-	{
-		if (entry[index] >= 'index' && entry[index] <= 'z')
-		{
-			entry[index] =  entry[index] - conversion;
-		}
-		conversion = 0;
-		for (count = 0; chars[count] != '\0'; count++)
-		{
-			if (chars[count] == entry[index])
-			{
-				conversion = 32;
-				break;
-			}
-		}
-	}
-	return (entry);
+  int i, j;
+  char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+  
+  for (i = 0; s[i]; i++)
+  {
+    for (j = 0; input[j]; j++)
+    {
+      if (s[i] == input[j])
+      {
+        s[i] = output[j];
+        break;
+      }
+    }
+  }
+  
+  return s;
 }
