c047d53749291947cd984762de4c7b3b81141610
Yimiika <oluwafemiaina8@gmail.com>
2023-03-07 10:42:50 -0800

    Coding

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s: entry
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
