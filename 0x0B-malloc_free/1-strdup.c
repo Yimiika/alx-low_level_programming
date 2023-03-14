f380b68e6e2fac92c856e338fa21ebd3d8788b9c
Yimiika <oluwafemiaina8@gmail.com>
2023-03-14 14:59:07 -0700

    Coding

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,27 +6,26 @@
  *
  * @str: This is the input string
  *
- * Return: function returns a pointer to the duplicated string. It returns 
- *         NULL if insufficient memory was available
+ * Return: function returns a pointer to the duplicated string
  */
 char *_strdup(char *str)
 {
-	char *s;
-	int a, b;
+        char *s;
+        int a, b;
 
-	if (str == NULL)
-		return (NULL);
+        if (str == NULL)
+                return (NULL);
 
-	for (a = 0; str[a] != '\0'; a++)
-		;
-	s = (char *)  malloc((a + 1) * sizeof(char));
+        for (a = 0; str[a] != '\0'; a++)
+                ;
+        s = (char *)  malloc((a + 1) * sizeof(char));
 
-	if (s == NULL)
-		return (NULL);
+        if (s == NULL)
+                return (NULL);
 
-	for (b = 0; b < a ; b++)
-		s[b] = str[b];
+        for (b = 0; b < a ; b++)
+                s[b] = str[b];
 
-	s[a + 1] = '\0';
-	return (s);
+        s[a + 1] = '\0';
+        return (s);
 }
