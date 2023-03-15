91faf5277ee63398bdfb0b074c86d4a65c23e084
Yimiika <oluwafemiaina8@gmail.com>
2023-03-15 06:57:09 -0700

    Coding

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Write a program that prints its name, followed by a new line.
- * If you rename the program, it will print the new name, without having to
- * compile it again
- * You should not remove the path before the name of the program
+ * main - Write a program that prints its name, followed by a new line
  *
  * @argc: This is the argument count
  * @argv: This is the argument vector
