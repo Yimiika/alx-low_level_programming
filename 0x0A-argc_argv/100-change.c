7bcbb2ebeba9e1b1093d26b93ed7ae4a89ec4a03
Yimiika <oluwafemiaina8@gmail.com>
2023-03-15 07:15:21 -0700

    Coding

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,56 +2,47 @@
 #include <stdlib.h>
 
 /**
- * main - program that prints coins left
- * argc - This is the command
- * argv - This is the array of commands
- *
- * Return: Always 0.
+ * main - Prints coints left
+ * @args: Paramater Entry
+ * @argv: One number of argumets
+ * Return: zero onOne number of argumetsr
  */
 
-int main(int argc, char *argv[])
+int main(int args, char *argv[])
 {
-int coins = 0, c;
+	int  c, coins = 0;
 
-if(argc != 2)
-{
-printf("Error\n")
-return(1);
-}
-c = atoi(argv[1]);
-if (c < 0)
-{
-printf("0\n")
-return(0);
-{
-for (c >= 0;)
-{
-if (c >= 25)
-{
-c -= 10;
-}
-else if (c >= 10)
-{
-c -= 10;
-}
-else if (c >= 5)
-{
-c -= 5;
-}
-else if (c >= 2)
-{
-c -= 2;
-}
-else if (c >= 1)
-{
-c -= 1;
-}
-else
-{
-break;
-coins += 1;
-}
-{
-printf("%d\n", coins);
-return(0);
+	if (args != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	c = atoi(argv[1]);
+	if (c < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (; c >= 0;)
+	{
+		if (c >= 25)
+			c -= 25;
+
+		else if (c >= 10)
+			c -= 10;
+
+		else if (c >= 5)
+			c -= 5;
+
+		else if (c >= 2)
+			c -= 2;
+
+		else if (c >= 1)
+			c -= 1;
+		else
+			break;
+		coins += 1;
+	}
+	printf("%d\n", coins);
+	return (0);
 }
