c283c333909e4291f5b8b238f8fd7b57ad30aacb
Yimiika <oluwafemiaina8@gmail.com>
2023-03-17 14:52:42 -0700

    Coding

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,78 +3,112 @@
 #include <string.h>
 #include <ctype.h>
 
-#define BUFFER_SIZE 1024
-
-void exit_error(const char* message) {
-    fprintf(stderr, "Error: %s\n", message);
+/**
+ * exit_error - function that exits the program with an error message
+ * @message: the error message to print
+ *
+ * Return: void
+ */
+void exit_error(char *message)
+{
+    printf("Error: %s\n", message);
     exit(98);
 }
 
-void validate_input(const char* num1, const char* num2) {
-    for (int i = 0; i < strlen(num1); i++) {
-        if (!isdigit(num1[i])) {
+/**
+ * validate_input - function that validates if the input is composed only of digits
+ * @num1: first number to validate
+ * @num2: second number to validate
+ *
+ * Return: void
+ */
+void validate_input(char *num1, char *num2)
+{
+    int i;
+    for (i = 0; num1[i] != '\0'; i++)
+    {
+        if (!isdigit(num1[i]))
+        {
             exit_error("Invalid input for num1");
         }
     }
-    for (int i = 0; i < strlen(num2); i++) {
-        if (!isdigit(num2[i])) {
+    for (i = 0; num2[i] != '\0'; i++)
+    {
+        if (!isdigit(num2[i]))
+        {
             exit_error("Invalid input for num2");
         }
     }
 }
 
-int multiply_digits(int digit1, int digit2, int* carry) {
-    int result = digit1 * digit2 + *carry;
-    *carry = result / 10;
-    return result % 10;
-}
-
-void multiply_numbers(const char* num1, const char* num2, char* result) {
+/**
+ * multiply_numbers - function that multiplies two numbers passed as strings
+ * @num1: the first number to multiply
+ * @num2: the second number to multiply
+ *
+ * Return: a string with the result of the multiplication
+ */
+char *multiply_numbers(char *num1, char *num2)
+{
     int len1 = strlen(num1);
     int len2 = strlen(num2);
-    int buffer_len = len1 + len2;
-    int* buffer = calloc(buffer_len, sizeof(int));
-    if (!buffer) {
-        exit_error("Failed to allocate memory");
+    int i, j, k;
+
+    char *result = calloc(len1 + len2 + 1, sizeof(char)); /* allocate memory for the result */
+    if (!result)
+    {
+        exit_error("Memory allocation error");
     }
 
-    for (int i = len1 - 1; i >= 0; i--) {
-        for (int j = len2 - 1; j >= 0; j--) {
-            int index1 = i + j;
-            int index2 = i + j + 1;
-            int digit1 = num1[i] - '0';
-            int digit2 = num2[j] - '0';
-            int product = multiply_digits(digit1, digit2, &buffer[index1]);
-            buffer[index2] += product;
+    /* multiply the numbers digit by digit and store the result in the result string */
+    for (i = len1 - 1; i >= 0; i--)
+    {
+        int carry = 0;
+        for (j = len2 - 1, k = i + len2; j >= 0; j--, k--)
+        {
+            int prod = (num1[i] - '0') * (num2[j] - '0') + carry + (result[k] - '0');
+            result[k] = (prod % 10) + '0';
+            carry = prod / 10;
         }
+        result[k] = carry + '0';
     }
 
-    int leading_zeroes = 0;
-    for (int i = 0; i < buffer_len; i++) {
-        if (buffer[i] > 0 || i == buffer_len - 1) {
-            leading_zeroes = i;
-            break;
-        }
+    /* remove leading zeros from the result string */
+    while (*result == '0' && *(result + 1) != '\0')
+    {
+        result++;
     }
 
-    for (int i = 0; i < buffer_len; i++) {
-        result[i] = buffer[i + leading_zeroes] + '0';
-    }
-    result[buffer_len - leading_zeroes] = '\0';
-
-    free(buffer);
+    return result;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
+/**
+ * main - function that multiplies two numbers passed as arguments
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ *
+ * Return: 0 on success, 98 on error
+ */
+int main(int argc, char **argv)
+{
+    char *num1, *num2, *result;
+
+    /* check if the correct number of arguments are passed */
+    if (argc != 3)
+    {
         exit_error("Invalid number of arguments");
     }
 
-    validate_input(argv[1], argv[2]);
+    num1 = argv[1];
+    num2 = argv[2];
 
-    char result[BUFFER_SIZE];
-    multiply_numbers(argv[1], argv[2], result);
+    validate_input(num1, num2);
+
+    result = multiply_numbers(num1, num2);
+
     printf("%s\n", result);
+
+    free(result); /* free the memory allocated for the result */
 
     return 0;
 }
