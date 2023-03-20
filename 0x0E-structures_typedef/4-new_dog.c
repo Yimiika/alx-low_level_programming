0ec22f897133bf34ce5ceb03e0a8893d6e7f6019
Yimiika <oluwafemiaina8@gmail.com>
2023-03-20 15:44:15 -0700

    coding

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,23 +11,35 @@
  * Return: a pointer to the new dog_t instance, or NULL if malloc fails
  */
 dog_t *new_dog(char *name, float age, char *owner) {
+    /* Allocate memory for the dog_t struct */
     dog_t *new_dog = malloc(sizeof(dog_t));
     if (new_dog == NULL) {
-        return NULL;
+        printf("Error: could not allocate memory for new dog.\n");
+        exit(1);
     }
+    
+    /* Allocate memory for the name and owner strings */
     new_dog->name = malloc(strlen(name) + 1);
     if (new_dog->name == NULL) {
+        printf("Error: could not allocate memory for name.\n");
         free(new_dog);
-        return NULL;
+        exit(1);
     }
-    strcpy(new_dog->name, name);
-    new_dog->age = age;
     new_dog->owner = malloc(strlen(owner) + 1);
     if (new_dog->owner == NULL) {
+        printf("Error: could not allocate memory for owner.\n");
         free(new_dog->name);
         free(new_dog);
-        return NULL;
+        exit(1);
     }
+    
+    /* Copy the name and owner strings */
+    strcpy(new_dog->name, name);
     strcpy(new_dog->owner, owner);
+    
+    /* Set the age field */
+    new_dog->age = age;
+    
+    /* Return a pointer to the new dog_t instance */
     return new_dog;
 }
