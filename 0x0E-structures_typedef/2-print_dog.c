1bf27be8c76c439dfbe2558008af083f7a31f743
Yimiika <oluwafemiaina8@gmail.com>
2023-03-20 15:13:32 -0700

    coding

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+/**
+ * print_dog - Prints a struct dog
+ * @d: Pointer to a struct dog variable
+ *
+ * Return: Void
+ */
+void print_dog(struct dog *d)
+{
+    if (d == NULL) {
+        return;
+    }
+
+    printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+    printf("Age: %.1f\n", d->age);
+    printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+}
