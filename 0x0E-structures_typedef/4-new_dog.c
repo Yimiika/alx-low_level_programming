477f9620e0d6d14d16552f2f96207743e8e70470
Yimiika <oluwafemiaina8@gmail.com>
2023-03-20 15:42:39 -0700

    coding

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,11 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
-
-typedef struct dog_s {
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
+#include "dog.h"
 
 /**
  * new_dog - creates a new dog_t instance with the given name, age, and owner
@@ -13,29 +8,26 @@ typedef struct dog_s {
  * @age: the age of the dog
  * @owner: the name of the dog's owner
  *
- * Return: a pointer to the newly created dog_t instance, or NULL if an error occurred
+ * Return: a pointer to the new dog_t instance, or NULL if malloc fails
  */
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *new_dog = malloc(sizeof(dog_t));
-
-	if (new_dog == NULL)
-		return (NULL);
-
-	new_dog->name = strdup(name);
-	if (new_dog->name == NULL) {
-		free(new_dog);
-		return (NULL);
-	}
-
-	new_dog->age = age;
-
-	new_dog->owner = strdup(owner);
-	if (new_dog->owner == NULL) {
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
-
-	return (new_dog);
+dog_t *new_dog(char *name, float age, char *owner) {
+    dog_t *new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL) {
+        return NULL;
+    }
+    new_dog->name = malloc(strlen(name) + 1);
+    if (new_dog->name == NULL) {
+        free(new_dog);
+        return NULL;
+    }
+    strcpy(new_dog->name, name);
+    new_dog->age = age;
+    new_dog->owner = malloc(strlen(owner) + 1);
+    if (new_dog->owner == NULL) {
+        free(new_dog->name);
+        free(new_dog);
+        return NULL;
+    }
+    strcpy(new_dog->owner, owner);
+    return new_dog;
 }
