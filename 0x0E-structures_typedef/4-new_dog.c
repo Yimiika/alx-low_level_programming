4e4b8c96e7b8a53a32e13ca0c0b8969c9a028e7a
Yimiika <oluwafemiaina8@gmail.com>
2023-03-20 15:58:35 -0700

    coding

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,38 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
 #include "dog.h"
-/**
-  * new_dog - creates a new dog
-  * @name: new dog's name
-  * @age: new dog's age
-  * @owner: new dog's owner
-  * Return: pointer to new dog struct
-**/
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *new_dog;
-	int i;
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-		return (NULL);
-	new_dog->name = malloc(sizeof(name) + 1);
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-	new_dog->owner = malloc(sizeof(owner) + 1);
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
-	for (i = 0; name[i]; i++)
-		new_dog->name[i] = name[i];
-	new_dog->name[i] = '\0';
-	new_dog->age = age;
-	for (i = 0; owner[i]; i++)
-		new_dog->owner[i] = owner[i];
-	new_dog->owner[i] = '\0';
-	return (new_dog);
+dog_t *new_dog(char *name, float age, char *owner) {
+    /* Check if name or owner is NULL */
+    if (name == NULL || owner == NULL) {
+        return NULL;
+    }
+    
+    /* Allocate memory for the new dog */
+    dog_t *new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL) {
+        return NULL;
+    }
+    
+    /* Allocate memory for the name and copy it */
+    new_dog->name = malloc(strlen(name) + 1);
+    if (new_dog->name == NULL) {
+        free(new_dog);
+        return NULL;
+    }
+    strcpy(new_dog->name, name);
+    
+    /* Allocate memory for the owner and copy it */
+    new_dog->owner = malloc(strlen(owner) + 1);
+    if (new_dog->owner == NULL) {
+        free(new_dog->name);
+        free(new_dog);
+        return NULL;
+    }
+    strcpy(new_dog->owner, owner);
+    
+    /* Set the age */
+    new_dog->age = age;
+    
+    /* Return the new dog */
+    return new_dog;
 }
