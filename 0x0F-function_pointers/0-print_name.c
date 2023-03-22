ff49738206eeb13c08a310daa25cd3489e7353ea
Yimiika <oluwafemiaina8@gmail.com>
2023-03-22 12:26:06 -0700

    Coding

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -8,24 +9,4 @@
 void print_name(char *name, void (*f)(char *))
 {
     f(name);
-}
-
-/**
- * print_with_hello - prints a greeting with the name
- * @name: pointer to a string containing the name
- */
-void print_with_hello(char *name)
-{
-    printf("Hello, %s!\n", name);
-}
-
-/**
- * main - entry point of the program
- * Return: 0 on success
- */
-int main(void)
-{
-    char name[] = "Alice";
-    print_name(name, print_with_hello); /* Output: Hello, Alice! */
-    return 0;
 }
