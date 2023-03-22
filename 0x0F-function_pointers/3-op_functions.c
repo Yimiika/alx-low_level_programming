0df794d0554317a112d1e8ec4c6deb5e0f6b395c
Yimiika <oluwafemiaina8@gmail.com>
2023-03-22 13:43:42 -0700

    Coding

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,71 +1,62 @@
 #include "3-calc.h"
 
 /**
- * op_add - Funcions sum two numbers
- *
- * @a: THis is the number 1
- * @b: This is the number 2
- *
- * Return: The result of the sum of two numbers
+ * op_add - function that adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result
  */
 
 int op_add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
 
 /**
- * op_sub - Funcions subtracion two numbers
- *
- * @a: THis is the number 1
- * @b: This is the number 2
- *
- * Return: The result of the subtracion of two numbers
+ * op_sub - function that adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result
  */
 
 int op_sub(int a, int b)
 {
-	return (a - b);
+return (a - b);
 }
 
 /**
- * op_mul - Funcions multiplaction two numbers
- *
- * @a: THis is the number 1
- * @b: This is the number 2
- *
- * Return: The result of the multiplation of two numbers
+ * op_mul - function that multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result
  */
 
 int op_mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
 
 /**
- * op_div - Funcions divition two numbers
- *
- * @a: THis is the number 1
- * @b: This is the number 2
- *
- * Return: The result of the divition of two numbers
+ * op_div - function that divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result
  */
 
 int op_div(int a, int b)
 {
-	return (a / b);
+return (a / b);
 }
 
 /**
- * op_mod - Funcions modulationo of two numbers
- *
- * @a: THis is the number 1
- * @b: This is the number 2
- *
- * Return: The result of the modulation of two numbers
+ * op_mod - function that modulates two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result
  */
+
 
 int op_mod(int a, int b)
 {
-	return (a % b);
+return (a % b);
 }
