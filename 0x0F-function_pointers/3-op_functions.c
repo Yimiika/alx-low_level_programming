38f4a92311681b1c8327ef83f355aa90e5fbbb95
Yimiika <oluwafemiaina8@gmail.com>
2023-03-22 13:40:31 -0700

    Coding

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,64 +1,71 @@
 #include "3-calc.h"
 
 /**
- * op_add - function that adds two integers
- * @a: first integer
- * @b: second integer
- * Return: the result
+ * op_add - Funcions sum two numbers
+ *
+ * @a: THis is the number 1
+ * @b: This is the number 2
+ *
+ * Return: The result of the sum of two numbers
  */
 
-int op_add(int a, int b);
+int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
 
 /**
- * op_sub - function that adds two integers
- * @a: first integer
- * @b: second integer
- * Return: the result
+ * op_sub - Funcions subtracion two numbers
+ *
+ * @a: THis is the number 1
+ * @b: This is the number 2
+ *
+ * Return: The result of the subtracion of two numbers
  */
 
-
-int op_sub(int a, int b);
+int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
 
 /**
- * op_mul - function that multiplies two integers
- * @a: first integer
- * @b: second integer
- * Return: the result
+ * op_mul - Funcions multiplaction two numbers
+ *
+ * @a: THis is the number 1
+ * @b: This is the number 2
+ *
+ * Return: The result of the multiplation of two numbers
  */
 
-
-int op_mul(int a, int b);
+int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
 
 /**
- * op_div - function that divides two integers
- * @a: first integer
- * @b: second integer
- * Return: the result
+ * op_div - Funcions divition two numbers
+ *
+ * @a: THis is the number 1
+ * @b: This is the number 2
+ *
+ * Return: The result of the divition of two numbers
  */
 
-int op_div(int a, int b);
+int op_div(int a, int b)
 {
-return (a / b);
+	return (a / b);
 }
 
 /**
- * op_mod - function that modulates two integers
- * @a: first integer
- * @b: second integer
- * Return: the result
+ * op_mod - Funcions modulationo of two numbers
+ *
+ * @a: THis is the number 1
+ * @b: This is the number 2
+ *
+ * Return: The result of the modulation of two numbers
  */
 
-
-int op_div(int a, int b);
+int op_mod(int a, int b)
 {
-return (a % b);
+	return (a % b);
 }
