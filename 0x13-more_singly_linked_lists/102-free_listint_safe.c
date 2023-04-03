8fc1e26d8242f22e15a3b37843df072aee23db62
Yimiika <oluwafemiaina8@gmail.com>
2023-04-03 10:13:16 -0700

    Coding

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,51 +1,55 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * free_listint_safe - Frees a listint_t list safely.
- * @h: Pointer to the head of the linked list.
+ * free_listint_safe - Frees a listint_t list
+ * @h: A pointer to the head of the list
  *
- * Return: The number of nodes freed.
+ * Return: The size of the list that was free'd
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count = 0;
-	listint_t *temp;
+    size_t size = 0;
+    listint_t *current, *tmp;
+    int flag;
 
-	if (!h || !(*h))
-		return (count);
+    if (h == NULL || *h == NULL)
+        return (0);
 
-	while (*h)
-	{
-		if (*h <= (*h)->next)
-		{
-			free(*h);
-			count++;
-			break;
-		}
+    current = *h;
 
-		temp = (*h)->next;
-		free(*h);
-		count++;
-		*h = temp;
+    while (current != NULL)
+    {
+        size++;
+        flag = 0;
+        if (current < current->next)
+        {
+            tmp = current->next;
+            current->next = NULL;
+            free(current);
+            current = tmp;
+            flag = 1;
+        }
+        else if (*h < current->next)
+        {
+            free(*h);
+            *h = NULL;
+            return (size);
+        }
+        else
+        {
+            tmp = current->next;
+            free(current);
+            current = tmp;
+            flag = 1;
+        }
 
-		if (!*h)
-			break;
+        if (flag == 0)
+            break;
+    }
 
-		if (*h >= (*h)->next)
-		{
-			free(*h);
-			count++;
-			break;
-		}
+    *h = NULL;
 
-		temp = (*h)->next;
-		free(*h);
-		count++;
-		*h = temp;
-	}
-
-	*h = NULL;
-
-	return (count);
+    return (size);
 }
