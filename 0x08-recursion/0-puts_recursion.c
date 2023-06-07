88bdd8bd770de26fa872e8426ab68acc15df0e70
Yimiika <oluwafemiaina8@gmail.com>
2023-06-07 11:27:58 -0700

    Coding

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -16,3 +16,21 @@ void _puts_recursion(char *s)
     _putchar(*s);
     _puts_recursion(s + 1);
 }
+#include "main.h"
+
+/**
+ * _puts_recursion - Function that prints a string followed by a new line
+ * @s: input string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	_putchar('\n');
+	return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
