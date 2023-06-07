0420b274852c2fa98463693334d6b0e475d3d233
Yimiika <oluwafemiaina8@gmail.com>
2023-06-07 13:37:56 -0700

    Coding

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+int primerecursion(int n, int i);
+/**
+ * is_prime_number- A function that checks if its a prime
+ * @n: input number
+ * Return: the value
+ */
+
+int is_prime_number(int n)
+{
+	if ( n <= 1)
+	return (0);
+	return primerecursion(n, 2)
+}
+
+/**
+ * primerecursion - function to check if a number is prime
+ * @n: input number
+ * @i: the divider to check
+ *
+ * Return: 1 if the number is prime
+ */
+int primerecursion(int n, int i)
+{
+if (i >= n)
+{
+return (1);
+}
+if (n % divisor == 0)
+{
+return 0;
+}
+return (primerecursion(n, i + 1));
+}
