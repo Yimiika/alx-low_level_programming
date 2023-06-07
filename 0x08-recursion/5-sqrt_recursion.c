bd61d83a304ba4f5066c1860b926aebf83e0f5a7
Yimiika <oluwafemiaina8@gmail.com>
2023-06-07 13:00:11 -0700

    Coding

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion- A function that returns the natural square root of a number
+ * @n: the input number
+ * Return: the value after calculation
+ */
+
+int _sqrt_recursion(int n)
+{
+	return sqrtfinder(n , 1);
+}
+
+/**
+ * sqrtfinder- A function that finds the natural square root of a number
+ * @n: the input number
+ * @i: first value
+ * Return: the value after calculation
+ */
+
+int sqrtfinder(int n, int i)
+{
+	if (i * i == n)
+	{
+	return (i);
+	}
+	if (i * i > n)
+{
+return (-1);
+}
+return sqrtfinder (n, i + 1);
+}
