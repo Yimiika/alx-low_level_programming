3cde688e9bf4f28ff3e4176718ff59e156833f33
Yimiika <oluwafemiaina8@gmail.com>
2023-06-07 11:36:39 -0700

    Coding

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,42 +1,6 @@
 #include "main.h"
 
 /**
- * _strchr - Function that prints a string followed by a new line
- * @s: input string
- */
-
-void _puts_recursion(char *s)
-{
-    if (*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
-
-    _putchar(*s);
-    _puts_recursion(s + 1);
-}
-#include "main.h"
-
-/**
- * _puts_recursion - Function that prints a string followed by a new line
- * @s: input string
- */
-
-void _puts_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-	_putchar('\n');
-	return;
-	}
-
-	_putchar(*s);
-	_puts_recursion(s + 1);
-}
-#include "main.h"
-
-/**
  * _puts_recursion - Prints a string followed by a new line
  * @s: input string
  */
