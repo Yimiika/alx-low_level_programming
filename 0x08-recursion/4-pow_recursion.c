d499f5aaa1d3e8d0d7eef3513f50310d2515d5ef
Yimiika <oluwafemiaina8@gmail.com>
2023-06-07 13:01:29 -0700

    Coding

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion- A function that returns the value of x raised to the power of y
+ * _pow_recursion- A function that returns the value of x raised to y
  * @x: first input number
  * @y: second input number
  * Return: the value after calculation
