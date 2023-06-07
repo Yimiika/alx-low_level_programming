58e5e4fb3e89bd0e778432c6ab9be6fd1f6ea89d
Yimiika <oluwafemiaina8@gmail.com>
2023-06-07 11:59:33 -0700

    Coding

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,22 +5,6 @@
  * @s: input string
  */
 
-void _puts_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-	return;
-	}
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
-}
-#include "main.h"
-
-/**
- * _print_rev_recursion - Prints a string in reverse
- * @s: input string
- */
-
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
