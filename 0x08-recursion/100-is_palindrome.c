085ba61ffd8d36c872ae963881de2f97bedda2bb
Yimiika <oluwafemiaina8@gmail.com>
2023-06-08 02:07:00 -0700

    Coding

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+int _strlen(char *s);
+int palinfinder(int s, int i);
+/**
+ * is_palindrome- function that tests if it's a palindrome
+ * @s: the input string
+ * Return: 1 if the string is a palindrome
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen(s);
+	return is_palindrome_recursive(s, 0, len - 1);
+}
+
+/**
+ * palinfinder- A function that finds if a string is a palindrome
+ * @s: the input number
+ * @i: first value
+ * Return: the value after calculation
+ */
+
+int sqrtfinder(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+	return (1);
+	}
+if (s[start]!= s[end])
+{
+return (0);
+}
+return (sqrtfinder(s, start + 1, end - 1));
+}
+
+/**
+ * _strlen- finds the length of a string
+ * @s: The string
+ *
+ * Return: The length of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	while (*s)
+	{
+	len++;
+	s++;
+	}
+	return len;
+}
