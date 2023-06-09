abf1a44a9d0848d7bd7c086a303e0cc2f1ad765c
Yimiika <oluwafemiaina8@gmail.com>
2023-06-09 08:10:01 -0700

    Coding

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- *main - the function
- *
+ *main - entry point of the program
  * @argc: returns the name of command or first command
  * @argv: the individual arguments
+ *
  * Return: returns zero
  */
 int main(int argc, char* argv[])
 {
-	int i, mul = 0;
-	for (argc < 2)
-	{
-		printf("Error\n");	
-	}
-	for (i = 1; i < argc; i++)
-	{
-		mul *= atoi(argv[i]);
-	}
-	return (0);
+int num1, num2, mul;
+
+if (argc < 3)
+{
+printf("Error\n");
+return (1);
+}
+num1 = atoi(argv[1]);
+num2 =atoi(argv[2]);
+mul = num1 * num2;
+printf("%d\n", mul);
+return (0);
 }
