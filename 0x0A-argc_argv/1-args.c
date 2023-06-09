ab188d76c1c00b54d6cd763023e7d55ad8fb759e
Yimiika <oluwafemiaina8@gmail.com>
2023-06-09 08:25:16 -0700

    Coding

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,13 @@
 
 /**
  * main - entry point of the program
- *
  * @argc: returns the name of command or first command
  * @argv: the individual arguments
+ *
  * Return: always zero
  */
 
-int main(int argc, char* argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
