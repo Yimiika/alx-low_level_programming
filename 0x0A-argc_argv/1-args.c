5d4a55a2dc668914db64651a5431a9fa004e65e2
Yimiika <oluwafemiaina8@gmail.com>
2023-06-09 07:24:11 -0700

    Coding

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *main - the function
+ *
+ * @argc: returns the name of command or first command
+ * @argv: the individual arguments
+ * Return: function returns a pointer to the duplicated string
+ */
+int main(int argc, char* argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
