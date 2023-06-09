8b77c8e4eaa345433dd18406dea76eb2b3055478
Yimiika <oluwafemiaina8@gmail.com>
2023-06-09 08:12:43 -0700

    Coding

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,26 @@
 #include "main.h"
 
 /**
- *main - entry point of the program
- * @argc: returns the name of command or first command
- * @argv: the individual arguments
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the arguments
  *
- * Return: returns zero
+ * Return: Always 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-int num1, num2, mul;
+	int num1, num2, mul;
 
-if (argc < 3)
-{
-printf("Error\n");
-return (1);
-}
-num1 = atoi(argv[1]);
-num2 =atoi(argv[2]);
-mul = num1 * num2;
-printf("%d\n", mul);
-return (0);
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
+
+	printf("%d\n", mul);
+	return (0);
 }
