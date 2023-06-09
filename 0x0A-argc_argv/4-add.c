cf8f752cc0f600d2fa21f9e5af71436d3ac947b0
Yimiika <oluwafemiaina8@gmail.com>
2023-06-09 09:05:11 -0700

    Coding

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 		int j;
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][j]))
+		if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
