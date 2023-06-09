0320536fb7bf402144b198cdbee4e1ac362ea59a
Yimiika <oluwafemiaina8@gmail.com>
2023-06-09 08:56:28 -0700

    Coding

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *main - entry point of the program
+ * @argc: returns the name of command or first command
+ * @argv: the individual arguments
+ * Return: returns zero
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, add = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);	
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		int j;
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+		}
+		printf("%d\n", add);
+		return (0);
+}
