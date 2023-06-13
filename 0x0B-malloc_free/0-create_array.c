9b82bc2f8b2a53c40cb1c204c51cb16c3c91b376
Yimiika <oluwafemiaina8@gmail.com>
2023-06-13 13:10:35 -0700

    coding

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * create_array - function that creates an array of chars
+ * @c : the character
+ *
+ * Return: returns NULL if 0 or fails
+ */
+char *create_array(unsigned int size, char c);
+{
+	if (size == 0)
+	{
+	return (NULL);
+	}
+	char *array = malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+	return (NULL);
+	}
+	for (unsigned int i = 0; i < size; i++)
+	array[i] = c;
+	return (array);
+}
