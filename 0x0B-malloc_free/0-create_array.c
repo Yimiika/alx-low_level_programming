38703f32e0ea7216caa22f267e51eace2586be5f
Yimiika <oluwafemiaina8@gmail.com>
2023-06-13 13:17:46 -0700

    coding

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,14 @@ char *create_array(unsigned int size, char c)
 	{
 	return (NULL);
 	}
-	char *array = malloc(size * sizeof(char));
+	char *array;
+	unsigned int i;
+	array = malloc(size * sizeof(char));
 	if (array == NULL)
 	{
 	return (NULL);
 	}
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	array[i] = c;
 	return (array);
 }
