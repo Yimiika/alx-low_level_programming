f7aaf8440dbd450a5e7c71794fb6c1ddf18c94c7
Yimiika <oluwafemiaina8@gmail.com>
2023-06-13 13:45:48 -0700

    coding

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strdup - function that creates a duplicate
+ * @str : the string to duplicate
+ *
+ * Return: returns pointer to duplicate
+ */
+
+char *_strdup(char *str)
+{
+	char *second;
+	int length = 0;
+	int i;
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	while (str[length] != '\0')
+{
+length++
+}
+	second = malloc((length + 1) * sizeof(char));
+	if (second == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	second[i] = str[i];
+	return (second);
+}
