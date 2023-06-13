50d31e27109f5f82ede784f09d57a6de9fb6c1b9
Yimiika <oluwafemiaina8@gmail.com>
2023-06-13 14:37:14 -0700

    coding

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * str_concat - function that cr
+ * @str : the string to duplicate
+ *
+ * Return: returns pointer to duplicate
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *concatenated;
+int length1 = 0, length2 = 0;
+int i, j;
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+while (s1[length1] != '\0')
+{
+length1++;
+}
+while (s2[length2] != '\0')
+{
+length2++;
+}
+concatenated = malloc((length1 + length2 + 1) * sizeof(char));
+if (concatenated == NULL)
+{
+return NULL;
+}
+for (i = 0; i < length1; i++)
+{
+concatenated[i] = s1[i];
+}
+for (j = 0; j < length2; j++)
+{
+concatenated[length1 + j] = s2[j];
+}
+concatenated[length1 + length2] = '\0';
+return concatenated;
+}
