70746243765c3850c9b3c25b21a9b6a1175b5b1b
Yimiika <oluwafemiaina8@gmail.com>
2023-06-13 14:50:30 -0700

    coding

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * str_concat - function that concatenate
+ * @s1 : the string to concatenate
+ * @s2 : the string to concatenate
+ *
+ * Return: returns pointer to concatenated
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+int **grid;
+int i, j;
+
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+grid = malloc(height * sizeof(int *));
+if (grid == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < length1; i++)
+{
+grid[i] = malloc(width * sizeof(int));
+if (grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
+{
+free(grid[j]);
+}
+free(grid);
+return (NULL);
+}
+for (j = 0; j < width; j++)
+{
+grid[i][j] = 0;
+}
+return (grid);
+}
