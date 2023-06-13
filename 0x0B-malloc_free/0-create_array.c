a7e191a0d327ab93390bd019c880bd72a00d5e2e
Yimiika <oluwafemiaina8@gmail.com>
2023-06-13 13:24:08 -0700

    coding

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,6 +3,7 @@
 /**
  * create_array - function that creates an array of chars
  * @c : the character
+ * @size: size of character
  *
  * Return: returns NULL if 0 or fails
  */
@@ -10,6 +11,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int i;
+
 	if (size == 0)
 	{
 	return (NULL);
