b1738bf3e5af211f1621d6064f1b6a1f05761e2b
Yimiika <oluwafemiaina8@gmail.com>
2023-06-15 12:56:49 -0700

    coding

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,37 +3,6 @@
 #include <stdio.h>
 
 /**
-  * _calloc - Allocates memory
-  * @nmemb: the number of elements
-  * @size: size of the element
-  *
-  * Return: Nothing.
-  */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	void *ptr;
-	unsigned int totalsize;
-
-	if (nmemb == 0 || size == 0)
-	{
-		return (NULL);
-	}
-	totalsize = nmemb * size;
-	ptr = malloc(totalsize);
-
-	if (ptr == NULL)
-	{
-		return (NULL);	
-	}
-	memset(ptr, 0, totalsize);
-	return (ptr);
-}
-
-#include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-
-/**
   * _realloc - reallocates a memory bock using malloc and free
   * @ptr: pointer to the previous memory
   * @old_size: size in bytes of old memory
