5068701317e8640a89e794591a993b7936610dfb
Yimiika <oluwafemiaina8@gmail.com>
2023-06-15 12:58:45 -0700

    coding

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,32 +12,31 @@
   */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *newptr
+	void *new_ptr;
 
 	if (new_size == old_size)
-	{
 		return (ptr);
-	}
+
 	if (ptr == NULL)
 	{
-		newptr = malloc(new_size);
-		if (newptr == NULL)
-		{
-			return(NULL);
-			return (newptr);
-		}
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+		return (new_ptr);
 	}
+
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	newptr = malloc(new_size);
-	if (newptr == NULL)
-	{
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
 		return (NULL);
-	}
-	memcpy(newptr, ptr, (old_size < new_size ? old_size : new_size));
+
+	memcpy(new_ptr, ptr, (old_size < new_size ? old_size : new_size));
 	free(ptr);
-	return (newptr);
+
+	return (new_ptr);
 }
