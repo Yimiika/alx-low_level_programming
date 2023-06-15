7184f1c15cd96e33eba70f2b32a298ae27a27441
Yimiika <oluwafemiaina8@gmail.com>
2023-06-15 09:08:13 -0700

    coding

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function that allocates memory
+ * @b: the value
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
