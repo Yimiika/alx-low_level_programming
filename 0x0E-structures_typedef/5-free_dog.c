fdb2abed62e61e26066cb660d46f8816d0fa2758
Yimiika <oluwafemiaina8@gmail.com>
2023-06-19 12:29:31 -0700

    coding

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -5,13 +5,14 @@
  * free_dog - Frees the memory allocated for a dog
  * @d: Pointer to the dog structure
  */
+
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		{
-			return;
-		}
-	free(d->name);
-    free(d->owner);
-    free(d);
+if (d == NULL)
+{
+return;
+}
+free(d->name);
+free(d->owner);
+free(d);
 }
