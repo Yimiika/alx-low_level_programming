a43ed8ea0dad9e680ea5c8ae7f05983a70dabb8b
Yimiika <oluwafemiaina8@gmail.com>
2023-06-19 12:03:25 -0700

    coding

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 
 /**
- * init_dog - initializes a variable of type struct dog
+ * print_dog - Prints the details of a dog
  * @d: dog
  *
  * Description: function that initializes struct of names
@@ -15,6 +15,6 @@ void print_dog(struct dog *d)
 			return;
 		}
 	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-	printf("Age: %.1f\n", d->age);
+	printf("Age: %.6f\n", d->age);
 	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
