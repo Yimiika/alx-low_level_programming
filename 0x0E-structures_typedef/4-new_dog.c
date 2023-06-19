5f9e6f56a50e7cc8d4654806a879f72c0376cbb4
Yimiika <oluwafemiaina8@gmail.com>
2023-06-19 12:54:08 -0700

    coding

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,39 +10,42 @@
  *
  * Return: Pointer to the newly created dog, or NULL if it fails
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog;
+    dog_t *new_dog;
+    char *name_copy, *owner_copy;
+    int name_length, owner_length;
 
-char *name_copy, *owner_copy;
+    name_length = strlen(name);
+    owner_length = strlen(owner);
 
-int name_length, owner_length;
+    new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+    {
+        return NULL;
+    }
 
-name_length = strlen(name);
-owner_length = strlen(owner);
-new_dog = malloc(sizeof(dog_t));
-if (new_dog == NULL)
-{
-return NULL;
-}
-name_copy = malloc((name_length + 1) * sizeof(char));
-if (name_copy == NULL)
-{
-free(new_dog);
-return NULL;
-}
-owner_copy = malloc((owner_length + 1) * sizeof(char));
-if (owner_copy == NULL)
-{
-free(name_copy);
-free(new_dog);
-return NULL;
-}
-strcpy(name_copy, name);
-strcpy(owner_copy, owner);
-new_dog->name = name_copy;
-new_dog->age = age;
-new_dog->owner = owner_copy;
-return (new_dog);
+    name_copy = malloc((name_length + 1) * sizeof(char));
+    if (name_copy == NULL)
+    {
+        free(new_dog);
+        return NULL;
+    }
+
+    owner_copy = malloc((owner_length + 1) * sizeof(char));
+    if (owner_copy == NULL)
+    {
+        free(name_copy);
+        free(new_dog);
+        return NULL;
+    }
+
+    strcpy(name_copy, name);
+    strcpy(owner_copy, owner);
+
+    new_dog->name = name_copy;
+    new_dog->age = age;
+    new_dog->owner = owner_copy;
+
+    return new_dog;
 }
