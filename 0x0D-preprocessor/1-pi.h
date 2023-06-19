3370f7a0ad5eaea6ec08966e3992e74e7674a895
Yimiika <oluwafemiaina8@gmail.com>
2023-06-19 07:35:19 -0700

    coding

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#define PI (3.141592653591024)
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry to the program
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	printf(%d\n, __FILE__);
+	return (0);
+}
