c659527f12434425d005b643fd3b689c37c288fa
Yimiika <oluwafemiaina8@gmail.com>
2023-06-19 10:34:06 -0700

    coding

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - gog features
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ *
+ * Description: struct of names
+ */
+struct dog
+{
+    char *name;
+    float age;
+	char *owner;
+};
