7c37e0ff9062f4a99b69a5ac3593263d22658b8f
Yimiika <oluwafemiaina8@gmail.com>
2023-06-21 14:36:15 -0700

    coding

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+#endif
