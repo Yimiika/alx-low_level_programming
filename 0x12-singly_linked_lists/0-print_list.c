ba7915b203d5f100543379fb12f8fe7a8751c0fd
Yimiika <oluwafemiaina8@gmail.com>
2023-07-03 09:40:31 -0700

    coding

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_list - Prints all the elements
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const struct list_s *h) 
+{
+  size_t count;
+
+  count = 0;
+  while (h != NULL)
+  {
+	printf("[%d] %s\n", count, h->str);
+    count++;
+    h = h->next;
+  }
+  return (count);
+}
