b3c44b23c1178fa39a3dc2de64906d3d91b33dd7
Yimiika <oluwafemiaina8@gmail.com>
2023-07-03 12:01:29 -0700

    coding

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,18 +7,22 @@
  * Return: the number of nodes
  */
 
-size_t print_list(const struct list_s *h) 
+size_t print_list(const struct list_s *h)
 {
   size_t count = 0;
 
 	while (h)
 	{
 		if (!h->str)
-			printf("[0] (nil)\n");
+			{
+				printf("[0] (nil)\n");
+			}
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			{
+				printf("[%u] %s\n", h->len, h->str);
+			}
 		h = h->next;
 		count++;
 	}
-  return (count);
+return (count);
 }
