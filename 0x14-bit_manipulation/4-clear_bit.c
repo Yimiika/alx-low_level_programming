248ed5c06f0a3b9797d2a5d621e47a2f43c5d5de
Yimiika <oluwafemiaina8@gmail.com>
2023-07-07 10:06:52 -0700

    coding

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,13 +7,14 @@
  *
  * Return: 1 if it worked, or -1 on error
  */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
-		return -1;
+    if (index >= sizeof(unsigned long int) * 8)
+        return -1;
 
-	unsigned long int mask = 1 << index;
-	mask = ~mask;
-	*n &= mask;
-	return 1;
+    unsigned long int mask = 1UL << index;
+    mask = ~mask;
+    *n &= mask;
+    return 1;
 }
