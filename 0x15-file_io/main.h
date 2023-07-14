9bc9a9c9797b6547aca89ee4e318deae8c6b739a
Yimiika <oluwafemiaina8@gmail.com>
2023-07-14 10:16:36 -0700

    coding

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include<string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
